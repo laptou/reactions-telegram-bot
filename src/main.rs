0a92de8bcb471014cda8783cb0b12e1471faffa3
Ibiyemi Abiodun <ibiyemi@intulon.com>
2021-01-10 13:55:21 -0500

    Add webhooks

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::env;
+use std::{convert::Infallible, env, net::SocketAddr};
 
-use log::{debug, info, warn};
+use log::{info, warn};
 use pretty_env_logger;
-use teloxide::utils::command::BotCommand;
+use teloxide::{dispatching::update_listeners::UpdateListener, utils::command::BotCommand};
 use teloxide::{prelude::*, types::*};
 use tokio;
 
 mod handler;
 mod reaction;
 
-use handler::{handle_callback_query, handle_command, handle_inline_query};
+use handler::{handle_callback_query, handle_command};
+use warp::{Filter, hyper::StatusCode};
 
 #[derive(BotCommand, PartialEq, Eq, Debug, Clone, Copy)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
@@ -38,6 +39,8 @@ async fn main() {
 
     let bot = Bot::from_env();
 
+    let listener = webhook(bot.clone()).await;
+
     Dispatcher::new(bot)
         .messages_handler(move |rx: DispatcherHandlerRx<Message>| {
             rx.for_each_concurrent(None, move |update| async move {
@@ -57,6 +60,61 @@ async fn main() {
                 }
             })
         })
-        .dispatch()
+        .dispatch_with_listener(listener, LoggingErrorHandler::new())
         .await;
+}
+
+pub async fn webhook<'a>(bot: Bot) -> impl UpdateListener<Infallible> {
+    // Heroku defines auto defines a port value
+    let teloxide_token = env::var("TELOXIDE_TOKEN").expect("TELOXIDE_TOKEN env variable missing");
+    let port: u16 = env::var("PORT")
+        .expect("PORT env variable missing")
+        .parse()
+        .expect("PORT value to be integer");
+    let path = format!("bot{}", teloxide_token);
+    let url = format!("https://bot.reaxnbot.dev/reaction/{}", path);
+
+    bot.set_webhook(url)
+        .send()
+        .await
+        .expect("Cannot setup a webhook");
+
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+
+    let server = warp::post()
+        .and(warp::path(path))
+        .and(warp::body::json())
+        .map(move |json: serde_json::Value| {
+            let try_parse = match serde_json::from_str(&json.to_string()) {
+                Ok(update) => Ok(update),
+                Err(error) => {
+                    log::error!(
+                        "Cannot parse an update.\nError: {:?}\nValue: {}\n\
+                       This is a bug in teloxide, please open an issue here: \
+                       https://github.com/teloxide/teloxide/issues.",
+                        error,
+                        json
+                    );
+                    Err(error)
+                }
+            };
+            if let Ok(update) = try_parse {
+                tx.send(Ok(update))
+                    .expect("Cannot send an incoming update from the webhook")
+            }
+
+            StatusCode::OK
+        })
+        .recover(handle_rejection);
+
+    let serve = warp::serve(server);
+
+    let address = format!("0.0.0.0:{}", port);
+    tokio::spawn(serve.run(address.parse::<SocketAddr>().unwrap()));
+    rx
+}
+
+async fn handle_rejection(error: warp::Rejection) -> Result<impl warp::Reply, Infallible> {
+    log::error!("Cannot process the request due to: {:?}", error);
+    Ok(StatusCode::INTERNAL_SERVER_ERROR)
 }
